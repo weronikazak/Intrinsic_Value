39579093fc8a1a0d161837355d5a3cc4ceb57f35
Weronika Żak <weronikarzak@gmail.com>
2022-06-01 00:36:51 +0100

    finish intr

---
+++ app.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template, Response, request
+
+app = Flask(__name__)
+
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+@app.route('/')
+def index():
+	return render_template('index.html', max=17000, labels=labels, values=values)
