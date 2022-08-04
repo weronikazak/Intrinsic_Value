d78e25217b00d9a37e598b7c5561e1a07171a193
Weronika Żak <weronikarzak@gmail.com>
2022-08-04 15:34:19 +0100

    add bars graphs

--- Vanguard/app.py
+++ Vanguard/app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 def index():
 	fund = list(get_fund_list().keys())[0]
 	df = fund_download(fund)
-	fund_to_plot = create_plot(df)
-	return render_template('index.html', plot=fund_to_plot)
+	graph_line, graph_bar = create_plot(df)
+	return render_template('index.html', plot_line=graph_line, plot_bar=graph_bar)
 
 
 def create_plot(df):
@@ -29,11 +29,22 @@ def create_plot(df):
                         marker_color='blue', 
 						text="Index Price"))
 
+	df_bars = df.groupby(df.Date.dt.year).mean()
+	bars_data = go.Figure(data=go.Bar(
+						x=df_bars.index, 
+                        y=df_bars[df_bars.columns[0]].values,
+                        marker_color='blue'))
+	bars_data.add_hline(y=df[df.columns[1]].mean(), line_dash="dot",
+              annotation_text=f"Mean: {df[df.columns[1]].mean()}", 
+              annotation_position="bottom right",
+              annotation_font_size=12,
+              annotation_font_color="black"
+             )
 
-	data = fund_data
 
-	graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-	return graphJSON
+	graph_line = json.dumps(fund_data, cls=plotly.utils.PlotlyJSONEncoder)
+	graph_bar = json.dumps(bars_data, cls=plotly.utils.PlotlyJSONEncoder)
+	return (graph_line, graph_bar)
 
 if __name__ == '__main__':
 	app.run()
