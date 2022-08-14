0f75a6a85d61be882c82e7af762d874dfacf549d
Weronika Żak <weronikarzak@gmail.com>
2022-08-14 09:13:19 +0100

    add plots

--- Vanguard/dashboard.py
+++ Vanguard/dashboard.py
@@ -5,152 +5,123 @@ from dash.dependencies import Input, Output, State
 
 import plotly.express as px
 
+from scrap import *
+
 # the style arguments for the sidebar.
 SIDEBAR_STYLE = {
-    'position': 'fixed',
-    'top': 0,
-    'left': 0,
-    'bottom': 0,
-    'width': '20vw',
-    'padding': '20px 10px',
-    'background-color': '#f8f9fa'
+	'position': 'fixed',
+	'top': 0,
+	'left': 0,
+	'bottom': 0,
+	'width': '20vw',
+	'padding': '20px 10px',
+	'background-color': '#f8f9fa',
+	'float': 'left'
 }
 
 # the style arguments for the main content page.
 CONTENT_STYLE = {
-    'width':'70vw',
-    'margin-left': '25vw',
-    'padding': '20px 10p'
+	# 'width':'70vw',
+	# 'margin-left': '25vw',
+	'padding': '20px 10p',
+	'float': 'left',
+	'position': 'relative'
 }
 
 TEXT_STYLE = {
-    'textAlign': 'center',
-    'color': '#191970'
+	'textAlign': 'center',
+	'color': '#191970'
 }
 
-CARD_TEXT_STYLE = {
-    'textAlign': 'center',
-    'color': '#0074D9'
-}
+dropdown_categories = [{'label': val, 'value': val} for val in list(get_fund_list().keys())]
 
 controls = html.Div(
-    [
-        html.P('Dropdown', style={
-            'textAlign': 'center'
-        }),
-        dcc.Dropdown(
-            id='dropdown',
-            options=[{
-                'label': 'Value One',
-                'value': 'value1'
-            }, {
-                'label': 'Value Two',
-                'value': 'value2'
-            },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
-            ],
-            value=['value1'],  # default value
-            multi=True
-        ),
-        html.Br(),
-        html.Button(
-            id='submit_button',
-            # n_clicks=0,
-            children='Submit',
-            # block=True
-        ),
-    ]
+	[
+		html.P('Dropdown', style={
+			'textAlign': 'center'
+		}),
+		dcc.Dropdown(
+			id='dropdown',
+			options=dropdown_categories,
+			value=dropdown_categories[0],  # default value
+			multi=True
+		),
+		html.Br(),
+		html.Button(
+			id='submit_button',
+			n_clicks=0,
+			children='Plot fund',
+		),
+	]
 )
 
 sidebar = html.Div(
-    [
-        html.H2('Parameters', style=TEXT_STYLE),
-        html.Hr(),
-        controls
-    ],
-    style=SIDEBAR_STYLE,
+	[
+		html.H2('Choose Funds', style=TEXT_STYLE),
+		html.Hr(),
+		controls
+	],
+	style=SIDEBAR_STYLE,
 )
 
 content_row = html.Div(
-        dcc.Graph(id='graph_4')
+	[
+		dcc.Graph(id='graph_timeseries'),
+		html.Br(),
+		dcc.Graph(id='graph_bars')
+	]
 )
 
 content = html.Div(
-    [
-        html.H2('Analytics Dashboard Template', style=TEXT_STYLE),
-        html.Hr(),
-        content_row,
-    ],
-    style=CONTENT_STYLE
+	[
+		html.H2('Index Funds', style=TEXT_STYLE),
+		html.Hr(),
+		content_row,
+	],
+	style=CONTENT_STYLE
 )
 
 app = dash.Dash(suppress_callback_exceptions=True,
-			external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css", "assets/style.css"])
-app.layout = html.Div([sidebar, content])
+				external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css", 
+				"assets/style.css"])
+app.layout = html.Div([sidebar, content], style={'overflowY':'scroll', 'float':'left'})
 
 
 @app.callback(
-    Output('graph_1', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)
-    fig = {
-        'data': [{
-            'x': [1, 2, 3],
-            'y': [3, 4, 5]
-        }]
-    }
-    return fig
+	Output('graph_timeseries', 'figure'),
+	[Input('submit_button', 'n_clicks')],
+	[State('dropdown', 'value')])
+def update_graph_timeseries(n_clicks, dropdown_value):
+	df = fund_download(dropdown_value['value'])
 
+	x = df[df.columns[0]].values
+	y = df[df.columns[1]].values
+
+	fig = px.line(df, x=x, y = y)
+	return fig
 
 @app.callback(
-    Output('graph_2', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_2(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)
-    fig = {
-        'data': [{
-            'x': [1, 2, 3],
-            'y': [3, 4, 5],
-            'type': 'bar'
-        }]
-    }
-    return fig
+	Output('graph_bars', 'figure'),
+	[Input('submit_button', 'n_clicks')],
+	[State('dropdown', 'value')])
+def update_graph_timeseries(n_clicks, dropdown_value):
+	df = fund_download(dropdown_value['value'])
 
+	df_bars = df.groupby(df.Date.dt.year).mean().reset_index()
 
-@app.callback(
-    Output('graph_6', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_6(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
-    df = px.data.tips()
-    fig = px.bar(df, x='total_bill', y='day', orientation='h')
-    return fig
+	x = df_bars[df_bars.columns[0]].values
+	y = df_bars[df_bars.columns[1]].values
 
+	fig = px.bar(df_bars, x=x, y = y)
+
+	# fig.update_traces(texttemplate='%{text:.2f}', textposition='inside')
+
+	fig.add_hline(y=df[df.columns[1]].mean(), line_dash="dot",
+						annotation_text=f"Mean: {round(df[df.columns[1]].mean(), 2)}", 
+						annotation_position="bottom left",
+						annotation_font_size=12,
+						annotation_font_color="black")
+	return fig
 
 if __name__ == '__main__':
-    app.run_server(port='8085')
+	app.run_server(port='8085')
